75749689a29ac27fe74c6a709c4e8c5fe6da43f9
FatemehNilchi <fatemehnilchi77@gmail.com>
2023-02-24 21:32:55 +0330

    DOM-get&set attributes&style

---
+++ script.js
@@ -0,0 +1,8 @@
+let h1titr1 = document.getElementById("titr");
+let h1titr = document.getElementsByClassName("texts");
+let titrs=document.querySelectorAll('.texts')
+console.log(titrs)
+h1titr[1].setAttribute("align", "center");
+console.log(h1titr1.getAttribute("id"));
+h1titr1.style.color = "red";
+console.log(h1titr1.style.color);
